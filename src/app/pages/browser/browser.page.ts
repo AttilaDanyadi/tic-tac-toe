67ce17729c912281902f1dada1362388883d1529
Dányádi Attila <Danyadi.Attila@vtep.videoton.hu>
2018-06-09 16:23:42 +0200

    mentett állások kezdetleges megjelenítése

--- src/app/pages/browser/browser.page.ts
+++ src/app/pages/browser/browser.page.ts
@@ -1,9 +1,23 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Board } from '../../models/board.model';
+import { DataProvider } from '../../providers/data.provider';
 
 @Component({
   selector: '[browser]',
   templateUrl: './browser.page.html',
   encapsulation: ViewEncapsulation.None,
 })
-export class BrowserPage {
+export class BrowserPage implements OnInit {
+  public Boards: Board[];
+
+  constructor(private dataProvider: DataProvider) { }
+
+  ngOnInit() {
+    this.dataProvider.GetBoard().subscribe(
+      (boards: Board[]) => {
+        this.Boards = boards;
+      },
+      (error) => console.log('error', error)
+    );
+  }
 }
